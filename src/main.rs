6f4b6399a524b9e24296e826cb34862c0247794e
Landon Gingerich <landon.ging@gmail.com>
2025-02-19 19:23:34 -0600

    fix: get stm32 export working

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 #![no_std]
 #![no_main]
 
+mod stm32;
+
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+use crate::stm32::Board;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
-    let mut led = Output::new(p.PC13, Level::Low, Speed::Low);
+    let mut led = Board::new();
 
     loop {
         info!("led on!");
         led.set_high();
-        Timer::after_secs(1).await;
-        
+        Timer::after_millis(500).await;
+
         info!("led off!");
         led.set_low();
-        Timer::after_secs(1).await;
+        Timer::after_millis(500).await;
     }
 }
